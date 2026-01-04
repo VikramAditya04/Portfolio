8a7845b7525e02fa48baed33fce4eb6f1360184c
Vikram Aditya <vikramaadi9798@gmail.com>
2026-01-04 16:54:55 +0530

    Vercel Analytics Added

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Home from './pages/HomePage.jsx'
+import { Analytics } from '@vercel/analytics/react';
+
 function App() {
 
   return (
   <>
     <Home />
+    <Analytics />
   </>
   )
 }
